1010d2d035f114e42dc7aaff5f21a668dee05914
Badr <baderalmasre330@gmail.com>
2024-07-04 15:40:14 +0400

    commit

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -102,7 +102,6 @@ class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateM
                           press: () {
                             // controller.changeIndex(index);
                             setState(() {
-
                             });
                           },
                         );
